0ca9350d6c7f712eee1d2b93a005efd40b89b2e0
rupert <re.paterson000@gmail.com>
2024-08-04 16:36:37 +1200

    RC-0: completed week 4 questions 1 and 2

---
+++ 159101_Applied_Programming/Week_4_Loops/1_display_name.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/**
+    Write a program to display your name 10 times, using a while loop, then using a for loop.
+ */
+
+int main() {
+    for(int i = 0; i < 10; i++) {
+        cout << "Rupert" << endl;
+    }
+}
---
+++ 159101_Applied_Programming/Week_4_Loops/2_descending_from_10.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/**
+    Write a program that displays the numbers 10 to 1 (descending order), using the most appropriate loop.
+ */
+
+int main() {
+    for(int i = 10; i > 0; i--) {
+        cout << i << endl;
+    }
+}
