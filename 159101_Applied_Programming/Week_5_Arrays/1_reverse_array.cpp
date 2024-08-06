d47596a48bae513d8e94fbab088d7b5ca508c3d7
rupert <re.paterson000@gmail.com>
2024-08-06 20:48:10 +1200

    RC-0: completed week 5 question 1

---
+++ 159101_Applied_Programming/Week_5_Arrays/1_reverse_array.cpp
@@ -0,0 +1,24 @@
+/**
+
+1. Write a program that uses a for loop to read in 7 real numbers and store them in an array. Then use
+another for loop to display the numbers in the reverse order. Do NOT sort.
+
+ */
+
+ #include <iostream>
+ using namespace std;
+
+float user_input;
+float input_array[7];
+
+int main() {
+    for (int i = 0; i < 7; i++) {
+        cout << "Please enter a number: ";
+        cin >> user_input;
+        input_array[i] = user_input;
+    }
+
+    for (int i = 6; i >= 0; i--) {
+        cout << input_array[i] << endl;
+    }
+}
